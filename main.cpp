c975d7996ff9afaba4f0bda7ab89f53cf53bc0d5
Bryan Albuja <balbujal@est.ups.edu.ec>
2022-05-31 10:34:19 -0500

    Creada la clase Persona

--- main.cpp
+++ main.cpp
@@ -1,9 +1,14 @@
 #include <iostream>
-
+#include "persona.h"
 using namespace std;
 
 int main()
 {
     cout << "Mi primer proyecto en Git!" << endl;
+    Persona p;
+    p.nombre = "Bryan";
+    p.edad = 19;
+    cout<< p.nombre <<endl;
+    cout<< p.edad <<endl;
     return 0;
 }
